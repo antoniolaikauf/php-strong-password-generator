e689367fcae0b08a87ee80b652115eee7cc8b8ef
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-01-25 11:00:00 +0100

    fix: sistemato condizione while in function

--- pageseparate/function.php
+++ pageseparate/function.php
@@ -5,19 +5,19 @@
 // $lenghtpassword lunghezza password decisa da utente
 function generatePassword($array, $arrayrisp, $lenghtpassword)
 {
-        $a = 0;
-        // ciclo infinito attenzione 
-        while ($a < 1) {
-            // generazione del numero 
-            $randomNumber = rand(0, count($array) - 1);
-            // controllo se il valore non è presente gia nell'array
-            if (!in_array($array[$randomNumber], $arrayrisp)) {
-                $arrayrisp[] = $array[$randomNumber];
-            }
-            // controllo 
-            if (count($arrayrisp) == $lenghtpassword) {
-                break;
-            }
+    // $a = 0;
+    // ciclo infinito attenzione 
+    while (count($arrayrisp) < $lenghtpassword) {
+        // generazione del numero 
+        $randomNumber = rand(0, count($array) - 1);
+        // controllo se il valore non è presente gia nell'array
+        if (!in_array($array[$randomNumber], $arrayrisp)) {
+            $arrayrisp[] = $array[$randomNumber];
         }
+        // controllo 
+        if (count($arrayrisp) == $lenghtpassword) {
+            break;
+        }
+    }
     return (implode('', $arrayrisp));
 }
